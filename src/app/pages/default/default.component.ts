545af37010d281a80dad87d3b09ceaf48d7230d6
Arthur <vilaca.arthur@yahoo.com.br>
2017-05-10 21:58:06 -0300

    menu component

---
+++ src/app/pages/default/default.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'default',
+  styleUrls: [ './default-component.css' ],
+  templateUrl: './default.component.html'
+})
+export class DefaultComponent {
+}
