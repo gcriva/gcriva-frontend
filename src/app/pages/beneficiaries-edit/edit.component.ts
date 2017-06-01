5833e5f0bc15e718b6655a8860b0b21442a17624
Arthur <vilaca.arthur@yahoo.com.br>
2017-05-31 21:03:24 -0300

    beneficiaries edit

--- src/app/pages/beneficiaries-edit/edit.component.ts
+++ src/app/pages/beneficiaries-edit/edit.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormControl } from '@angular/forms';
 import { AuthHttp } from 'angular2-jwt';
 import { MdSnackBar } from '@angular/material';
 import { AppState } from '../../app.service';
 import { handleErrorResponse } from '../../utils';
-import {FormControl} from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 
-import * as jsonBrasil from '../../json/cities.json'
+import * as jsonBrasil from '../../json/cities.json';
 
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
@@ -31,25 +30,29 @@ export class EditBeneficiariesComponent {
 	) {
 		this.stateCtrl = new FormControl();
 		this.states = jsonBrasil.estados;
-		this.filteredStates = this.stateCtrl.valueChanges
-			.startWith(null)
-			.map(name => this.filterStates(name));
+		this.filterS();
 
-		this.route.params.subscribe(params => {
+		this.route.params.subscribe((params) => {
 			this.editObjt = this.appState.state.beneficiaries.filter((beneficiarie) => {
-				return beneficiarie.id == params['id'];
+				return beneficiarie.id === params['id'];
 			})[0];
 		});
+	}
+
+	public filterS() {
+		// this.filteredStates = this.states.filter((state) => { return state.nome.indexOf(this.editObjt.state) > -1 });
 	}
 
 	public save() {
 		this.http.post('/beneficiaries', this.editObjt)
 			.subscribe((data) => {
-				this.router.navigate(['beneficiarios']).then(_ => {});
-			}, handleErrorResponse(this.snackBar));
+					this.router.navigate(['beneficiarios']);
+				},
+				handleErrorResponse(this.snackBar)
+			);
 	}
 
 	public filterStates(val: string) {
-		return val ? this.states.filter(s => new RegExp(`^${val}`, 'gi').test(s)) : this.states;
+		return val ? this.states.filter((s) => new RegExp(`^${val}`, 'gi').test(s)) : this.states;
 	}
 }
