19ec7c4a1a0e9cb30cb366540a0bd43ee45beafc
wagoid <w.rsantos@outlook.com>
2017-05-31 22:23:35 -0300

    Fix projects edit not saving

--- src/app/pages/projects-edit/edit.component.ts
+++ src/app/pages/projects-edit/edit.component.ts
@@ -10,41 +10,41 @@ import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/map';
 
 @Component({
-	selector: 'projects-edit',
-	styleUrls: ['./edit-components.scss'],
-	templateUrl: './edit.components.html'
+  selector: 'projects-edit',
+  styleUrls: ['./edit-components.scss'],
+  templateUrl: './edit.components.html'
 })
 export class EditProjectsComponent {
-	public editObjt: any = {};
-	constructor(
-		public http: AuthHttp,
-		public snackBar: MdSnackBar,
-		private appState: AppState,
-		private router: Router,
-		private route: ActivatedRoute
-	) {
-		this.route.params.subscribe((params) => {
-			this.editObjt = this.appState.state.projects.filter((project) => {
-				return project.id === params['id'];
-			})[0];
-		});
-	}
+  public editObjt: any = {};
+  constructor(
+    public http: AuthHttp,
+    public snackBar: MdSnackBar,
+    private appState: AppState,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.params.subscribe((params) => {
+      this.editObjt = this.appState.state.projects.filter((project) => {
+        return project.id === params['id'];
+      })[0];
+    });
+  }
 
-	public save() {
-		if (this.editObjt.id) {
-			this.http.put('/projects/' + this.editObjt.id, this.editObjt)
-				.subscribe((data) => {
-						this.router.navigate(['projetos']);
-					},
-					handleErrorResponse(this.snackBar)
-				);
-		} else {
-			this.http.post('/projects', this.editObjt)
-				.subscribe((data) => {
-						this.router.navigate(['projetos']);
-					},
-					handleErrorResponse(this.snackBar)
-				);
-		}
-	}
+  public save() {
+    if (this.editObjt.id) {
+      this.http.put('/projects/' + this.editObjt.id, { project: this.editObjt })
+        .subscribe((data) => {
+            this.router.navigate(['projetos']);
+          },
+          handleErrorResponse(this.snackBar)
+        );
+    } else {
+      this.http.post('/projects', { project: this.editObjt })
+        .subscribe((data) => {
+            this.router.navigate(['projetos']);
+          },
+          handleErrorResponse(this.snackBar)
+        );
+    }
+  }
 }
